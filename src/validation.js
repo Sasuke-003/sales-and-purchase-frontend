e63b470a2aef69d21912b3066a61754bd6f97c3b
NEURON <50287102+neuron909@users.noreply.github.com>
2020-07-11 10:48:33 +0530

    validation update

--- src/validation.js
+++ src/validation.js
@@ -1,18 +1,15 @@
-const config = () => {
-    module.exports._validator = {
-        User : {
-            _email    : { min: 1, max : 10 },
-            _pass     : { min: 1, max : 10 },
-            _fullName : { min: 1, max : 10 },
-            _type     : { min: 1, max : 1  },
-        },
-        Item : {
-            _name : { min : 1, max : 10 },
-            _unit : { min : 1, max : 10 },
-        },
-        Seller : {
-            _name : { min : 1, max : 10 },
-        }
+module.exports._validator = {
+    User : {
+        _email    : { min: 1, max : 10 },
+        _pass     : { min: 1, max : 10 },
+        _fullName : { min: 1, max : 10 },
+        _type     : { min: 1, max : 1  },
+    },
+    Item : {
+        _name : { min : 1, max : 10 },
+        _unit : { min : 1, max : 10 },
+    },
+    Seller : {
+        _name : { min : 1, max : 10 },
     }
 }
-config();
